3ff8a0132fcc67564df3f657216ef646640cd8d5
Bhavik-Sanghar <130123723+Bhavik-Sanghar@users.noreply.github.com>
2025-07-16 09:02:44 +0530

    test: add failing test for deleting sweet by ID

--- test/test_sweet_service.py
+++ test/test_sweet_service.py
@@ -11,3 +11,13 @@ def test_add_sweet_successfully():
 
     assert len(all_sweets) == 1
     assert all_sweets[0].name == "Kaju Katli"
+
+def test_delete_sweet_by_id():
+    service = SweetService()
+    sweet = Sweet(1001, "Kaju Katli", "Nut-Based", 50.0, 20)
+    service.add_sweet(sweet)
+
+    service.delete_sweet(1001)
+    all_sweets = service.get_all_sweets()
+
+    assert len(all_sweets) == 0
