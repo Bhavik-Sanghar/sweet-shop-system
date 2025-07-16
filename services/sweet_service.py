40ee1e29df8b4c646fb9f45128a997fa85dd9a86
Bhavik-Sanghar <130123723+Bhavik-Sanghar@users.noreply.github.com>
2025-07-16 08:56:19 +0530

    test: add failing test for add_sweet functionality

---
+++ services/sweet_service.py
@@ -0,0 +1,9 @@
+class SweetService:
+    def __init__(self):
+        pass
+
+    def add_sweet(self, sweet):
+        pass
+
+    def get_all_sweets(self):
+        return []
---
+++ test/test_sweet_service.py
@@ -0,0 +1,13 @@
+import pytest
+from services.sweet_service import SweetService
+from models.sweet import Sweet
+
+def test_add_sweet_successfully():
+    service = SweetService()
+    sweet = Sweet(1001, "Kaju Katli", "Nut-Based", 50.0, 20)
+    
+    service.add_sweet(sweet)
+    all_sweets = service.get_all_sweets()
+
+    assert len(all_sweets) == 1
+    assert all_sweets[0].name == "Kaju Katli"
